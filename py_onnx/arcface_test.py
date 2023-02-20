6d581d91a4a4de173cd0e853512f9a845029fd57
clone-boy <htmaaaaaa@gmail.com>
2023-02-20 21:22:54 +0800

    modify model export

--- py_onnx/arcface_test.py
+++ py_onnx/arcface_test.py
@@ -3,24 +3,34 @@ import cv2
 from iresnet import iresnet50
 import onnxruntime as ort
 
-device = 'cuda'
-model = iresnet50()
-model.load_state_dict(torch.load('ckp/w600k_r50.pth'))
-model.eval().to(device)
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, help='initial weights path')
+    parser.add_argument('--save_onnx', type=str, help='saved onnx path')
+    parser.add_argument('--img_path', type=str, help='img path')
 
-image_path = 'img/test.jpg'
-img = cv2.imread(image_path)
-img = img[:, :, ::-1]
-img = torch.Tensor(img.copy()).unsqueeze(0).permute(0, 3, 1, 2).to(device)
-img.div_(255).sub_(0.5).div_(0.5)
+    return parser.parse_args()
+
+def main(opt):
+    device = 'cuda'
+    model = iresnet50()
+    model.load_state_dict(torch.load(opt.weights))
+    model.eval().to(device)
+
+    image_path = opt.img_path
+    img = cv2.imread(image_path)
+    img = img[:, :, ::-1]
+    img = torch.Tensor(img.copy()).unsqueeze(0).permute(0, 3, 1, 2).to(device)
+    img.div_(255).sub_(0.5).div_(0.5)
 
 
-feature = model(img)
-print(feature)
+    feature = model(img)
+    print(feature)
 
-ort_session = ort.InferenceSession('ckp/arcface.onnx',providers=['CUDAExecutionProvider'])
+    ort_session = ort.InferenceSession(opt.save_onnx,providers=['CUDAExecutionProvider'])
+    feature = ort_session.run(None, { 'image' : img.cpu().numpy() } )
+    print(feature)
 
-
-feature = ort_session.run(None, { 'image' : img.cpu().numpy() } )
-print(feature)
-
+if __name__ == '__main__':
+    opt = parse_opt()
+    main(opt)
--- py_onnx/export_onnx.py
+++ py_onnx/export_onnx.py
@@ -1,15 +1,26 @@
 import torch
 from iresnet import iresnet50
 
-if __name__ == '__main__':
-    device = 'cuda'
-    model = iresnet50()
-    model.load_state_dict(torch.load('ckp/w600k_r50.pth'))
-    model.eval().to(device)
-    input = torch.randn(1, 3, 112, 112).to(device)
-    torch.onnx.export(
-            model, input, 'arcface.onnx',
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, help='initial weights path')
+    parser.add_argument('--save_onnx', type=str, help='saved onnx path')
+
+    return parser.parse_args()
+
+def main(opt):
+	device = 'cuda'
+	model = iresnet50()
+	model.load_state_dict(torch.load(opt.weights))
+	model.eval().to(device)
+	input = torch.randn(1, 3, 112, 112).to(device)
+	torch.onnx.export(
+            model, input, opt.save_onnx,
             input_names=['image'],
             output_names=['feature'],
             dynamic_axes={'image': {0: 'batch'}, 'feature': {0: 'batch'}}
     )
+
+if __name__ == '__main__':
+    opt = parse_opt()
+    main(opt)
